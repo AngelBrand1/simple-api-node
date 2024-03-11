20171624209e7f57cd5c358c7ea9d45d1455d28e
Angel <angeldavidbran000@gmail.com>
2024-03-11 12:44:40 -0500

    jwt

--- api/routes/user.js
+++ api/routes/user.js
@@ -2,8 +2,38 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
+
+router.get('/', (req, res, next) => {
+    User
+    .find()
+    .select('email _id')
+    .exec()
+    .then(docs => {
+        const response = {
+            count: docs.length,
+            users: docs.map(doc => {
+                return {
+                    email: doc.email,
+                    _id: doc._id,
+                    request: {
+                        type: 'GET',
+                        url: 'http://localhost:3000/user/' + doc._id
+                    }
+                }
+            })
+        };
+        res.status(200).json(response);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    });
+});
 
 router.post('/signup', (req, res, next) => {
     User
@@ -40,6 +70,57 @@ router.post('/signup', (req, res, next) => {
             res.status(500).json({
                 error: err
             });
+        });
+    });
+});
+
+router.post('/login', (req, res, next) => {
+    User
+    .find({ email: req.body.email })
+    .exec()
+    .then(user => {
+        if (user.length < 1) {
+            return res.status(401).json({
+                message: 'Auth failed'
+            });
+        }
+        bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+            console.log('result', result);
+            console.log('err', err);
+            if(err) {
+                return res.status(401).json({
+                    message: err.message
+                });
+            }
+            if (!result) {
+                return res.status(401).json({
+                    message: 'Auth failed'
+                });
+            }
+            if (result) {
+                jwt.sign({
+                    email: user[0].email,
+                    userId: user[0]._id
+                }, process.env.JWT_KEY, {
+                    expiresIn: '1h'
+                }, (err, token) => {
+                    if (err) {
+                        return res.status(401).json({
+                            message: 'Auth failed'
+                        });
+                    }
+                    return res.status(200).json({
+                        message: 'Auth successful',
+                        token: token
+                    });
+                })
+            }
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
         });
     });
 });
